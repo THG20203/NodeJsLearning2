4fd26e88ad70e4349d7c9f6bc2cd6d78160e2104
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-21 23:44:56 +0100

    test

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+/* superagent module, a popular library for making HTTP requests. */
+const superagent = require("superagent");
+
+/* This function reads the content of a file named dog.txt located in
+the same directory as the script (__dirname is a Node.js global variable 
+that holds the directory path of the current module). The readFile 
+function is asynchronous and takes a callback function that executes 
+once the file read operation is complete or if it encounters an error.
+err: If an error occurs during the file read operation, this parameter will 
+contain an error object. data: If the file is read successfully, this 
+parameter contains the contents of the file. */
+fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
+    console.log(`Breed: ${data}`);
+
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random Fetch!   `)
+});
