5a26619d3940c558bec13ccd63d3f368dac2f909
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-22 12:44:36 +0100

    add catch method

--- index.js
+++ index.js
@@ -14,14 +14,15 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     console.log(`Breed: ${data}`);
 
     superagent
-    .get(`https://dog.ceo/api/breed/${data}/images/random`)
-    .end((err, res) => {
-        if (err) return console.log(err.message)
+    .get(`https://dog.ceo/api/breed/${data}/images/random`).then(res => {
         console.log(res.body.message);
 
         /* callback function to save in new file */
         fs.writeFile("dog-img.txt", res.body.message, err => {
-            console.log("Rondon dog image saved to file");
+            if (err) return console.log(err.message);
+            console.log("Random dog image saved to file");
         });
-    });
+    }).catch(err => {
+        console.log(err.message);
+    })
 });
