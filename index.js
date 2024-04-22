e72448782e181d2f91986a8b1c00682f70df6aa1
THG20203 <tristanhgriffiths8@yahoo.com>
2024-04-22 11:55:01 +0100

    callback in a callback so need for promises

--- index.js
+++ index.js
@@ -16,9 +16,12 @@ fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
     superagent
     .get(`https://dog.ceo/api/breed/${data}/images/random`)
     .end((err, res) => {
+        if (err) return console.log(err.message)
         console.log(res.body.message);
 
         /* callback function to save in new file */
-        fs.writeFile()
+        fs.writeFile("dog-img.txt", res.body.message, err => {
+            console.log("Rondon dog image saved to file");
+        });
     });
 });
